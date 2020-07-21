6486dd3a0e6436676ac4523743a1aec79c7857fc
Kamal Nasser <kamal@digitalocean.com>
2020-07-22 00:28:17 +0300

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You're running React on App Platform!
         </p>
         <a
           className="App-link"
