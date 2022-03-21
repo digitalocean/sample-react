fc848f7c0969349530d120d1dce90a57f6358245
David Vandal <dsv101@gmail.com>
2022-03-21 14:14:25 -0400

    change app name to vandalino-family

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ export default function App() {
       </div>
 
       <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Your New React App{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
+        Welcome to the union of David Vandal and Alescia Coppolino
       </h1>
 
       <div className="grid grid-cols-2 grid-rows-2 gap-4">
