a878675297d28d53e6bafddac026c75094dab3b8
David Vandal <dsv101@gmail.com>
2022-03-24 11:36:40 -0400

    use typescript and sass

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders david vandal', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/david vandal/i);
-  expect(linkElement).toBeInTheDocument();
-});
