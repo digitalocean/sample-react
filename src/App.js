a4d5d274a62926e9aa68d23706add24c3a48bda2
Emily Marie Ahtúnan <86572370+Emmarie-Ahtunan@users.noreply.github.com>
2023-10-17 06:59:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
       </div>
 
       <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Welcome to Your New React App{" "}
+        My First Digital Ocean React App{" "}
         <span className="block text-lg text-blue-400">on DigitalOcean</span>
       </h1>
 
